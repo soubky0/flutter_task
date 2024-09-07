0c250cc27955b1196398574b6bbdd77e4702403f
soubky0 <omar.soubky@proton.me>
2024-09-07 15:17:53 +0300

    feat: listening, tracking, displaying working on android

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-- [ ] Listen for calls in the background.
-- [ ] Track the call start and end times to calculate the duration.
-- [ ] Display the call duration once the call is completed.
+- [x] Listen for calls in the background.
+- [x] Track the call start and end times to calculate the duration.
+- [x] Display the call duration once the call is completed.
 - [ ] Ensures the app functions on both Android and iOS.
 - [ ] Write README.
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,124 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:phone_state/phone_state.dart';
+import 'package:workmanager/workmanager.dart';
 
-void main() {
-  runApp(const MyApp());
+const task = "call_listener";
+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    return Future.value(true);
+  });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+main() {
+  Workmanager().initialize(callbackDispatcher);
+  Workmanager().registerPeriodicTask(
+    "1",
+    task,
+    frequency: const Duration(minutes: 15),
+  );
 
-  // This widget is the root of your application.
+  runApp(
+    const MaterialApp(
+      home: Example(),
+    ),
+  );
+}
+
+class Example extends StatefulWidget {
+  const Example({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<Example> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  PhoneState status = PhoneState.nothing();
+  bool granted = false;
+  DateTime? callStartTime;
+
+  // Function to request permission for phone access
+  Future<bool> requestPermission() async {
+    var status = await Permission.phone.request();
+
+    // Handling different permission states
+    return switch (status) {
+      PermissionStatus.denied ||
+      PermissionStatus.restricted ||
+      PermissionStatus.limited ||
+      PermissionStatus.permanentlyDenied =>
+        false,
+      PermissionStatus.provisional || PermissionStatus.granted => true,
+    };
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void initState() {
+    super.initState();
+    requestPermission().then((isGranted) {
+      if (isGranted) {
+        setStream();
+      }
+    });
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  // Function to listen to the phone state stream
+  void setStream() {
+    PhoneState.stream.listen((event) {
+      setState(() {
+        status = event;
+        if (status.status == PhoneStateStatus.CALL_STARTED) {
+          callStartTime = DateTime.now();
+          Timer.periodic(const Duration(seconds: 1), (Timer t) {
+            setState(() {});
+          });
+        }
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Phone State'),
+        centerTitle: true,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if (status.status == PhoneStateStatus.CALL_STARTED)
+              const Text(
+                'Ongoing call',
+                style: TextStyle(fontSize: 24),
+              ),
+            if (status.status == PhoneStateStatus.CALL_STARTED)
+              Text(
+                "Call Duration: ${formatDuration()}",
+                style: const TextStyle(fontSize: 24),
+              )
+            else
+              const Text(
+                "Listening for calls...",
+                style: TextStyle(fontSize: 24),
+              ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  // Function to format the call duration to seconds
+  String formatDuration() {
+    final duration = DateTime.now().difference(callStartTime!);
+
+    final minutes = duration.inMinutes.toString().padLeft(2, '0');
+    final seconds = (duration.inSeconds % 60).toString().padLeft(2, '0');
+
+    return "$minutes:$seconds";
   }
 }
